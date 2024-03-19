67bee405b5f9803f007c9f82f30cbee54fcb16cb
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-03-19 10:33:36 -0300

    Criando as Models

---
+++ SmartSchool/SmartSchool.API/Models/Professor.cs
@@ -0,0 +1,17 @@
+﻿namespace SmartSchool.API.Models
+{
+    public class Professor
+    {
+        public Professor() { }
+
+        public Professor(int id, string? nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public IEnumerable<Disciplina>? Disciplinas { get; set; }
+    }
+}
