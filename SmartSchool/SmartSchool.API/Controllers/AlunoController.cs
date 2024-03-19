0764edb5eacd7051bbcd30150d55db890fdda3de
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-03-19 12:07:15 -0300

    Controllers e o HTTP Get

--- SmartSchool/SmartSchool.API/Controllers/AlunoController.cs
+++ SmartSchool/SmartSchool.API/Controllers/AlunoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SmartSchool.API.Models;
 
 namespace SmartSchool.API.Controllers
 {
@@ -7,13 +8,49 @@ namespace SmartSchool.API.Controllers
     [ApiController]
     public class AlunoController : ControllerBase
     {
+
+        public List<Aluno>? Alunos = new List<Aluno>()
+        {
+            new Aluno()
+            {
+                Id = 1,
+                Nome = "Marcos",
+                Sobrenome= "Almeida",
+                Telefone= "3874-7451"
+            },
+             new Aluno()
+            {
+                Id = 2,
+                Nome = "João",
+                Sobrenome="Barbosa",
+                Telefone= "3874-7448"
+            },
+              new Aluno()
+            {
+                Id = 3,
+                Nome = "Paulo",
+                Sobrenome="Ferreira",
+                Telefone= "3874-7435"
+            }
+        };
+
         public AlunoController() { }
 
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Alunos: Paulo, Marta, Laura, Lucas");
+            return Ok(Alunos);
         }
-       
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Id == id );
+            if (aluno == null)
+                return BadRequest("O aluno não foi encontrado"); 
+
+            return Ok(aluno);          
+        }
+
     }
 }
