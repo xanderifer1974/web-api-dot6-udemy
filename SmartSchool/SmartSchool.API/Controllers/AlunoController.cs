370730eb94f0bfee0ef3ec1473d61b1816db6031
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-03-19 11:07:29 -0300

    Criando as controllers

---
+++ SmartSchool/SmartSchool.API/Controllers/AlunoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartSchool.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+        public AlunoController() { }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Alunos: Paulo, Marta, Laura, Lucas");
+        }
+       
+    }
+}
---
+++ SmartSchool/SmartSchool.API/Controllers/ProfessorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartSchool.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfessorController : ControllerBase
+    {
+        public ProfessorController() { }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Professores: Paulo, Marta, Laura, Lucas");
+        }
+
+    }
+}
