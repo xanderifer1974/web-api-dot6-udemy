8dc0d4971d9f6c488227678e3c7e8fddfeb6c76a
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-03-28 12:05:50 -0300

    Criando as paginas com os títulos

--- SmartSchool/SmartSchoolApp/src/app/app.module.ts
+++ SmartSchool/SmartSchoolApp/src/app/app.module.ts
@@ -4,26 +4,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ToastrModule } from 'ngx-toastr';
-import { NgxSpinnerModule } from 'ngx-spinner';
+
+import { AlunosComponent } from './components/alunos/alunos.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { ProfessoresComponent } from './components/professores/professores.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { TituloComponent } from './components/shared/titulo/titulo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunosComponent,
+    DashboardComponent,
+    PerfilComponent,
+    ProfessoresComponent,
+    NavComponent,
+    TituloComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    NgxSpinnerModule,
-    ToastrModule.forRoot({
-      timeOut: 3500,
-      positionClass: 'toast-bottom-right',
-      preventDuplicates: true,
-      progressBar: true,
-      closeButton: true
-    })
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- SmartSchool/SmartSchoolApp/src/app/components/shared/titulo/titulo.component.ts
+++ SmartSchool/SmartSchoolApp/src/app/components/shared/titulo/titulo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-titulo',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TituloComponent implements OnInit {
 
+  @Input() titulo: string;
   constructor() { }
 
   ngOnInit() {
